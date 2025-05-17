c4772b6283949319ef58d9a55820e775b2d33d0f
kavyashreenayak29 <kavyashreenayak8861@gmail.com>
2025-05-17 21:24:20 +0530

    Update StringOperations.java

--- StringOperations.java
+++ StringOperations.java
@@ -45,3 +45,27 @@ public class StringOperations {
         System.out.println("Email ends with '.edu.in': " + email.endsWith(".edu.in"));
     }
 }
+
+
+//2.b. Develop a Java program to make a Performance Test on StringBuffer and StringBuilder for appending the string ‘AIET’ for 10000 times both in the StringBuffer and StringBuilder. Justify your answer which one is better.
+public class StringBufferVsStringBuilder {
+    public static void main(String[] args) {
+        long startTime, endTime;
+
+        StringBuffer stringBuffer = new StringBuffer();
+        startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            stringBuffer.append("AIET");
+        }
+        endTime = System.nanoTime();
+        System.out.println("StringBuffer time: " + (endTime - startTime) + " ns");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            stringBuilder.append("AIET");
+        }
+        endTime = System.nanoTime();
+        System.out.println("StringBuilder time: " + (endTime - startTime) + " ns");
+    }
+}
